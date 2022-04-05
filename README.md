48c82c96fad36837cfd210b1891f98c35c609b2d
pgann2 <>
2022-04-05 16:25:24 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# _You must follow the comments for your input, processing and output for credit._
+
+# _There should only be ONE output._
+
+# Correct Change
+
+Write a Python program to assist a cashier with determining correct change. The program should have one input, the number of cents to return to the customer. The output should be the appropriate change in quarters, dimes, nickels and cents.    
+
+
+For an example input of 41 cents your output would look like this:  
+
+```
+Here is your change: 
+1 Quarter 
+1 Dime 
+1 Nickel 
+1 Penny
+```
+
+HINTS
+
+- User integer values for your inputs and change values. (enter in 25 rather than **.**25 as a decimal)
+- Review the week 2 PowerPoint for math operators
+- _Do not worry about making the output plural_, UNLESS you already know how to do this.
+- _Do not worry about data validation_, UNLESS you already know how to do this. 
---
+++ main.py
@@ -0,0 +1,69 @@
+#Title: Correct Change 
+#Name: Paul Gann
+#Date: 04 April 2022
+#Purpose of Program: Correctly calculate the correct denominations of coin change to be returned to the customer for the amount of change input.
+#User enters in a change value between 1 and 99
+#Program outputs the number of quarters, dimes, nickels, and pennies to return to the user.
+
+#INPUT-----------------------------------------------------------------
+#create a variable named change, ask the user for the change value, be sure these are number data types
+
+print()  # A greeting to print only at the start of the program (outside of any loop).
+print("Welcome to 'Correct Change'.")
+change = input("Please enter the amount of change to calculate: ")  # Request change amount to be calculated from the user
+# Validate input - only integers allowed
+try:
+    int(change) # Validating the input is an integer; if not skips to the exception at the end.
+    change = int(change)  # Convert user input to an integer
+    if change not in range(1,100):  # Verify user input conforms with parameters
+        print()
+        print("I am sorry; you entered " + str(change) + " which is more than 100 cents.")
+        print("Please re-run and enter an integer between 1 and 99.")
+        quit()
+    else:  
+        # PROCESSING------------------------------------------------------------
+        #Develop assignment statements to figure the number of quarters, dimes, nickels, and pennies
+
+        #quarters
+        quarters = change // 25
+        left_over_after_quarters = change % 25
+
+        #dimes
+        dimes = left_over_after_quarters // 10
+        left_over_after_dimes = left_over_after_quarters % 10
+
+        #nickels
+        nickels = left_over_after_dimes // 5
+        left_over_after_nickels = left_over_after_dimes % 5
+
+        #pennies
+        pennies = left_over_after_nickels // 1
+
+     #OUTPUT-----------------------------------------------------------------
+     #The number of quarters, dimes, nickels, pennies.
+        print()
+        print("```")
+        print("Here is your change: ") 
+        if quarters > 1:
+            print (str(quarters) + " x Quaters")
+        else:
+            print (str(quarters) + " x Quarter") 
+        if dimes > 1:
+            print (str(dimes) + " x Dimes")
+        else:
+            print (str(dimes) + " x Dime") 
+
+        print (str(nickels) + " x Nickel")  # Skipping plural Nickels as that would be Dimes
+        
+        if pennies > 1:
+            print (str(pennies) + " x Pennies")
+        else:
+            print (str(pennies) + " x Penny") 
+    print()
+    print("```")
+    quit()
+
+except ValueError:  # If user enters a non-integer, print an error and exit.
+    print("Invalid input.  Please re-run, and try again with an integer between 1 and 99.")
+    quit()
+  
